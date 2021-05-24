1b1e34bffcbb1a71682a96e22bc70fce15c4ef90
Sherbrooke Andrews <sherbrooke@grablabs.com>
2021-05-24 17:20:23 -0400

    adding env info

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const util = require('util');
+
 const app = express();
+ 
  
 const PORT = 3000;
  
+ console.log(process.env);
+ 
 app.get('/', function (req, res) {
-  res.send(`Hello World!<br />${new Date().toGMTString()}<br />Test push!`);
+  res.send(`Hello World!<br />${new Date().toGMTString()}<br />Test push!<br /><pre>${util.inspect(process.env)}</pre>`);
 });
  
 app.listen(PORT, () => {
