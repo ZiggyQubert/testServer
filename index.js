a4752d04966b59888b0a69a0698d86eb0b48213f
Sherbrooke Andrews <sherbrooke@grablabs.com>
2021-05-24 17:35:30 -0400

    adding info endpoint

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const util = require('util');
+const http = require('http-request');
 
 const app = express();
- 
- 
+
+
 const PORT = 3000;
- 
- console.log(process.env);
- 
-app.get('/', function (req, res) {
+
+console.log(process.env);
+
+app.get('/', (req, res) => {
   res.send(`Hello World!<br />${new Date().toGMTString()}<br />Test push!<br /><pre>${util.inspect(process.env)}</pre>`);
 });
- 
+
+app.get('/info', (req, res) => {
+  if (process.env.ECS_CONTAINER_METADATA_URI_V4) {
+    http.get(process.env.ECS_CONTAINER_METADATA_URI_V4, function (err, res) {
+      if (err) {
+        res.error(err);
+        return;
+      }
+      res.send(res.buffer.toString());
+    });
+  } else {
+    res.send('Info not available');
+  }
+})
+
+
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
